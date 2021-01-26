acab3f8f8f6712e43a85256813fc235d8423d210
NguyenNhat <vnctdlgt@gmail.com>
2021-01-26 21:39:09 +0700

    NVN

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # OOP
+![Dethi](https://user-images.githubusercontent.com/58680643/105859106-b8c2ed80-601e-11eb-98ec-5bbb49e4f3a6.jpg)
---
+++ src/nguyenvannhat/KNCSDL.java
@@ -0,0 +1,27 @@
+package nguyenvannhat;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author hocvien
+ */
+public class KNCSDL {
+    public static Connection layKetNoi(){
+        Connection kn=null;
+        String user="sa";
+        String pass="123";
+        String uRL="jdbc:sqlserver://;databaseName=QLSACH_TiKi";
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            kn=DriverManager.getConnection(uRL, user, pass);
+            System.out.println("Connected");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Disconnected");
+        }
+        return kn;
+    }
+    
+}
